9eaea41631dc1cdb4ce0e31c404f18f375e22e52
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-05-12 20:28:49 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
